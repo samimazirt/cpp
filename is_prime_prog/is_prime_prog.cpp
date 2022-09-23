ac6b74f0091261c144ed85f909d68fbbef6e7f8d
Sami Mazirt <sami.mazirt@epita.fr>
2022-09-23 21:09:30 +0200

    isprime

---
+++ is_prime_prog/is_prime_prog.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+void is_prime(int k) {
+    if (k == 1) {
+        std::cout << k << " is a prime: True" << std::endl;
+        return;
+    }
+    for (int i = 2; i < k/2; i++) {
+        if (k%i == 0) {
+            std::cout << k << " is a prime: False" << std::endl;
+            return;
+        }
+    }
+    std::cout << k << " is a prime: True" << std::endl;
+}
+
+int main() {
+    std::string nums;
+    std::getline(std::cin, nums);
+    std::string num = "";
+
+    for(long unsigned int k = 0; k < nums.length(); k++) {
+        if (isdigit(nums[k])) {
+            num += nums[k];
+        }
+        if (nums[k+1] == ' ' || nums[k+1] == '\0') {
+            is_prime(stoi(num));
+            num = "";
+        }
+    }
+    return 0;
+
+
+}
